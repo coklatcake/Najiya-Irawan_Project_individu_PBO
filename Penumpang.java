d1bcce9c8c3ed62af2d2b1c9bbf2596f4eb97cd4
Najiya Irawan <najiyairawan06@gmail.com>
2025-12-02 15:05:26 +0700

    Add constructor to Penumpang class

--- Penumpang.java
+++ Penumpang.java
@@ -4,3 +4,10 @@ public class Penumpang {
     private int usia;
     private boolean kondisiHamil;
     private int saldoKartu;
+
+public Penumpang(String nama, int usia, boolean kondisiHamil) {
+        this.nama = nama;
+        this.usia = usia;
+        this.kondisiHamil = kondisiHamil;
+        this.saldoKartu = 10000; // saldo awal default
+    }
