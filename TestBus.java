a01cb17f52cf820b89047ad23d48a75a374178cc
Najiya Irawan <najiyairawan06@gmail.com>
2025-12-02 15:08:52 +0700

    Add TestBus class with main method

---
+++ TestBus.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TestBus {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Bus bus = new Bus();
+
+        boolean jalan = true;
