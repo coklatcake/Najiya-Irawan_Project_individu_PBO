972690c098cce6b8dea18dda3983bf988f2732d5
Najiya Irawan <najiyairawan06@gmail.com>
2025-12-02 15:06:26 +0700

    Add methods for saldo management and priority check

--- Penumpang.java
+++ Penumpang.java
@@ -11,3 +11,31 @@ public Penumpang(String nama, int usia, boolean kondisiHamil) {
         this.kondisiHamil = kondisiHamil;
         this.saldoKartu = 10000; // saldo awal default
     }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getUsia() {
+        return usia;
+    }
+
+    public boolean isHamil() {
+        return kondisiHamil;
+    }
+
+    public int getSaldo() {
+        return saldoKartu;
+    }
+
+    public void tambahSaldo(int jumlah) {
+        saldoKartu += jumlah;
+    }
+
+    public void bayarOngkos(int ongkos) {
+        saldoKartu -= ongkos;
+    }
+
+    public boolean termasukPrioritas() {
+        return usia > 60 || usia < 10 || kondisiHamil;
+    }
